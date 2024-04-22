467a87c5abd443c5fc8fc6188fb8d57c19f7f780
Artix Kai <artix@github.com>
2024-04-22 19:44:26 +0530

    change structure

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ set -x
 CFLAGS="$(pkg-config --cflags raylib)"
 LDFLAGS="$(pkg-config --libs raylib)"
 
-for i in *.c;do
+for i in src/*.c;do
   clang $i $CFLAGS $LDFLAGS -c -o obj/$i.o
 done
 
-clang obj/*.o $CFLAGS $LDFLAGS -o main && ./main
+clang obj/*.o $CFLAGS $LDFLAGS -o obj/main && obj/main
